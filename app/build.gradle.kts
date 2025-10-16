acac38f8e148c99ebf969361d8ee2a5dc348ffd2
Rafal Kociniewski <rafal.kociniewski@gmail.com>
2025-10-16 10:08:20 +0200

    docs: CONTRIBUTING.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "rk.powermilk.clown"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.3"
+        versionCode = 5
+        versionName = "1.1.4"
         buildToolsVersion = "36.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
