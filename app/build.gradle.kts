d90e49ce1441ee41fd1183b2907af9b2c441edff
Rafal Kociniewski <rafal.kociniewski@gmail.com>
2025-10-16 10:11:50 +0200

    ci: qodana

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "rk.powermilk.clown"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.4"
+        versionCode = 6
+        versionName = "1.1.5"
         buildToolsVersion = "36.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
